407b43d3aee6a578ee17ba0466df694be9c5600c
cccchiban <dartskydog40@gmail.com>
2024-09-24 21:30:03 +0800

    Update BCSB.sh

--- BCSB.sh
+++ BCSB.sh
@@ -91,19 +91,32 @@ kernel_optimization() {
     echo "正在编辑 /etc/sysctl.conf 文件..."
     
     sudo bash -c 'cat << EOF >> /etc/sysctl.conf
-net.ipv4.tcp_window_scaling = 1
-net.core.rmem_max = 33554432
-net.core.wmem_max = 33554432
-net.ipv4.tcp_rmem = 4096 131072 33554432
-net.ipv4.tcp_wmem = 4096 16384 33554432
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=1
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_fastopen=3
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=4096 131072 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
-net.ipv4.tcp_adv_win_scale = -2
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_timestamps = 1
-kernel.panic = -1
-vm.swappiness = 0
-net.ipv4.tcp_fastopen = 3
+vm.swappiness=10
+kernel.panic=10
 EOF'
 
     echo "应用配置..."
